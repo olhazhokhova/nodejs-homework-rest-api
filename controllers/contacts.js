410698b3f32e7754a118de3586b2a4a5c850cb87
Olha Zhokhova <zhokhova.olha@gmail.com>
2022-06-12 21:40:56 +0300

    fix limit

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,9 +4,10 @@ const { Contact } = require("../models/contact");
 const listContacts = async (req, res, next) => {
   try {
     const {_id} = req.user;
-    const {page = 1, limit = 10} = req.query;
-    const skip = (page - 1) * limit;
-    const contacts = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate("owner", "_id email subscription");
+    const {page = 1, limit = 20} = req.query;
+    const skipped = (page - 1) * limit;
+    const skip = skipped < 0 ? 0 : skipped;
+    const contacts = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate("owner", "_id email subscription");    
     res.json({
       status: "success",
       code: 200,
